7355e8aad493f469cfd7feaa0d82ecd66b80218d
Andrew Lin <andrew.lin@ieee.org>
2023-02-18 19:09:06 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,51 @@
+# Read a Paper
+
+Choose a random paper (that hasn't been chosen recently) from the Papers We Love
+repo on Github and suggest it somewhere, be it on the command line (useful for
+debugging), or Discord (or any other site with a simple webhook).
+
+Inspired by the [love-a-paper](https://github.com/imwally/love-a-paper) bot that
+used to post to Twitter.
+
+The assumed use case is this project is run on an always-on Linux device 
+(e.g. Raspberry Pi) with Internet access on a periodic basis (e.g. via cron).
+
+## Use
+
+Clone the Love a Paper repo somewhere. The default location is below this
+directory.
+
+```shell
+# With Github CLI:
+$ gh clone love-a-paper/love-a-paper
+
+# With Git:
+$ git clone https://github.com/papers-we-love/papers-we-love.git
+```
+
+Run the `suggest_a_paper.jl` script from the command line to see it in action.
+
+```shell
+$ julia suggest_a_paper.jl
+```
+
+Or run it from a cron job via the helper script. While you're there, set up a 
+cron job to pull the Papers we Love repo to keep it up to date. The example 
+shows updates happening on the 13th of the monthly at 01:00, and paper 
+suggestions daily at 18:30.
+
+```shell
+# Launch crontab editor for user account:
+$ crontab -e
+
+# Add to crontab file:
+*  1  13 *  * /path/to/project/update_papers
+30 18 *  *  * /path/to/project/suggest_a_paper
+```
+
+See https://crontab.guru for help setting up crontab.
+
+## Dependencies
+
+* Julia 1.8+
+* git
---
+++ suggest_a_paper.jl
@@ -0,0 +1,167 @@
+import HTTP
+
+
+"""
+Paper information.
+"""
+struct Paper
+    title::AbstractString
+    url::AbstractString
+end
+
+"""
+Repo information.
+"""
+struct Repo
+    path::AbstractString
+    url::AbstractString
+end
+
+"""
+Load recent papers from `path`.
+"""
+function recent_papers(path::AbstractString)
+    isfile(path) ? readlines(path) : Vector{AbstractString}()
+end
+
+"""
+Write `depth` most recent papers to `path`.
+"""
+function update_recent_papers(
+    path::AbstractString, 
+    recent_papers::Vector{<:AbstractString}, 
+    latest_paper::Paper, 
+    depth::Integer
+)
+    all_papers = first([ [latest_paper.url] ; recent_papers ], depth)
+    open(path, "w") do f
+        for p in all_papers
+            println(f, p)
+        end
+    end
+end
+
+"""
+Create the base URL in repo for path.
+
+path = /path/to/repo/subdir
+repo.path = /path/to/repo
+repo.url = https://github.com/love_a_paper/love_a_paper
+returns https://github.com/love_a_paper/love_a_paper/subdir
+"""
+function link_url(path::AbstractString, repo::Repo)
+    subdir = path[begin + length(repo.path):end]
+    repo.url * "/blob/main" * subdir
+end
+
+"""
+Extract PDF links listed in README.
+"""
+function papers_in_readme(root::AbstractString, file::AbstractString, repo::Repo)
+    papers = Vector{Paper}()
+    base_url = link_url(root, repo)
+    for line in eachline(joinpath(root, file))
+        m = match(r"\[(.*)\]\((.*)\)", line)
+        if isnothing(m) || ! endswith(m[2], ".pdf")
+            continue
+        end
+            
+        title = m[1]
+        url = startswith(m[2], "http") ? m[2] : "$base_url/$(m[2])"
+        push!(papers, Paper(title, url))
+    end
+
+    return papers
+end
+
+"""
+Find all papers in repo.
+"""
+function repo_papers(repo::Repo)
+    papers = Vector{Paper}()
+    for (root, dirs, files) in walkdir(repo.path)
+        if root == repo.path
+            # Skip root directory.
+            continue
+        end
+        for f in files
+            if f == "README.md"
+                append!(papers, papers_in_readme(root, f, repo))
+                break;
+            end
+        end
+    end
+
+    return papers
+end
+
+"""
+Randomly select a paper that hasn't been chosen recently.
+"""
+function choose_paper(
+    papers::Vector{Paper}, 
+    recent_papers::Vector{<:AbstractString}
+)
+    while true
+        p = rand(papers)
+        if p.url ∉ recent_papers
+            return p
+        end
+    end
+end
+
+"""
+Suggest paper to endpoint.
+"""
+function suggest(paper::Paper)
+    println("How about reading '$(paper.title)' @ $(paper.url)?")
+end
+
+function suggest(paper::Paper, url::AbstractString)
+    println("Suggesting '$(paper.title)' to Discord.")
+
+    headers = [
+        "Content-Type" => "application/json", 
+        "Accept" => "application/json"
+    ]
+    body = """{
+        "content": "How about reading '$(paper.title)' @ $(paper.url)?"
+    }"""
+    
+    # Server will silently fail if something goes wrong, so ignore the response.
+    HTTP.post(url, headers, body)
+
+    return
+end
+
+"""
+Entry point.
+"""
+function main()
+    depth = 100
+    paper_history = abspath("./paper_history.txt")
+    repo = Repo(
+        abspath("./papers-we-love"), 
+        "https://github.com/papers-we-love/papers-we-love"
+    )
+    discord_webhook = readline("discord_webhook.txt")
+    
+    previously_suggested_papers = recent_papers(paper_history)
+    all_papers = repo_papers(repo)
+    if length(all_papers) == 0
+        # Something went wrong. Ask for help.
+        suggest(Paper("read-a-paper needs some TLC.", ""), discord_webhook)
+    else
+        chosen_paper = choose_paper(all_papers, previously_suggested_papers)
+        update_recent_papers(
+            paper_history, 
+            previously_suggested_papers, 
+            chosen_paper,
+            depth
+        )
+        suggest(chosen_paper, discord_webhook)
+    end
+end
+
+# Suggest a Paper.
+main()
